e7fbcc43030f6af2eaa7a4a7c106f5d57978a22f
joan <nutsukpuijoan@gmail.com>
2025-06-02 15:41:18 +0200

    add second_dir

---
+++ lib/secondPart/signUp.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class signUp extends StatelessWidget {
+  const signUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Padding(padding: EdgeInsets.all(9),
+          child: Column(
+            children: [
+              Text("Sign in"),
+
+            ],
+          ),
+          )
+        ],
+      ),
+    );
+  }
+}
